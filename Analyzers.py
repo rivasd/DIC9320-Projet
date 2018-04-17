5b64072b994923012d99d0ceb3c34ea1ae986d8b
rivasd <rivasdaniel1992@gmail.com>
2018-04-17 11:35:59 -0400

    xml parsing of TREC files

---
+++ Analyzers.py
@@ -0,0 +1,5 @@
+import lucene
+from org.apache.pylucene.analysis import PythonAnalyzer
+from  org.apache.lucene.analysis.core import WhitespaceAnalyzer
+
+BaselineAnalyzer = WhitespaceAnalyzer
--- Indexer.py
+++ Indexer.py
@@ -1,6 +1,33 @@
 import lucene
 import os
+import re
+import xml.etree.ElementTree as ET
+from Analyzers import BaselineAnalyzer
+from java.nio.file import Paths
+from org.apache.lucene.store import SimpleFSDirectory
+from org.apache.lucene.document import Document, Field, StringField, TextField
+from org.apache.lucene.index import \
+    FieldInfo, IndexWriter, IndexWriterConfig, IndexOptions
+import org.apache.pylucene
+
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
+
+if not os.path.exists("./Indexes/Baseline"):
+    os.mkdir("./Indexes/Baseline")
+
+store       = SimpleFSDirectory(Paths.get("./Indexes/Baseline"))
+analyzer    = BaselineAnalyzer()
+config      = IndexWriterConfig(analyzer)
+config.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+writer = IndexWriter(store, config)
+
+
 
 for TrecFile in os.listdir("./AP"):
     with open(os.path.abspath(os.path.join("AP",TrecFile)), 'r') as docfile:
-        docfile
+        contents    = docfile.read()
+        xml         = '<ROOT>' + contents + "</ROOT>"
+        root        = ET.fromstring(xml)
+        pass
+
